76e6aab723f092e50cdd449c9e07b7478576d1ac
Caribla <caribla4@gmail.com>
2024-05-02 16:11:51 +0200

    Feat: Post character endpoint

--- src/entities/character/controller.js
+++ src/entities/character/controller.js
@@ -1,19 +1,40 @@
-import Character from "./Character";
+import Character from "./Character.js";
 
 export const getAllCharacters = async (req, res) => {
-    const characters = await Character.find({});
-    return res.status(200).json({
-        success: true,
-        message: "Characters retrieved succesfully",
-        data: characters
-    })
+    try {
+        const characters = await Character.find({});
+        return res.status(200).json({
+            success: true,
+            message: "Characters retrieved succesfully",
+            data: characters
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 export const postCharacter = async (req, res) => {
-    return res.status(200).json({
-        success: true,
-        message: "Characters posted succesfully"
-    })
+    try {
+        const { name, image, unlock } = req.body;
+
+        if(!name || !image || !unlock){
+            throw new Error ("No provided correct data for post a character");
+        }
+
+        const newCharacter = await Character.create({
+            name,
+            image,
+            unlock
+        });
+        
+        return res.status(200).json({
+            success: true,
+            message: "Characters posted succesfully",
+            data: newCharacter
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 export const updateCharacterById = async (req, res) => {
