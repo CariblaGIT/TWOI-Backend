47d48bb1b6f4b7291bdd74f7cc62826df5cf248f
Caribla <caribla4@gmail.com>
2024-05-04 13:05:28 +0200

    Feat: Update item by id endpoint

--- src/entities/item/controller.js
+++ src/entities/item/controller.js
@@ -41,11 +41,46 @@ export const postItem = async (req, res) => {
 }
 
 export const updateItemById = async (req, res) => {
-    return res.status(200).json({
-        success: true,
-        message: "Item updated succesfully",
-        //data: itemToUpdate
-    });
+    try {
+        const { name, quote, description, type, icon } = req.body;
+        const itemId = req.params.id;
+
+        if(!name && !quote && !description && !type && !icon){
+            throw new Error ("No provided data to update a item");
+        }
+
+        const itemToUpdate = await Item.findById(itemId);
+
+        if(name){
+            itemToUpdate.name = name;
+        }
+
+        if(quote){
+            itemToUpdate.quote = quote;
+        }
+
+        if(description){
+            itemToUpdate.description = description;
+        }
+
+        if(type){
+            itemToUpdate.type = type;
+        }
+
+        if(icon){
+            itemToUpdate.icon = icon;
+        }
+
+        itemToUpdate.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Item updated succesfully",
+            data: itemToUpdate
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 export const deleteItemById = async (req, res) => {
