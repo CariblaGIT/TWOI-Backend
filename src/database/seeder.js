bd2cbafa8577893f52957696c3aa4f3552a3bea3
Caribla <caribla4@gmail.com>
2024-05-07 14:37:07 +0200

    Fix: Testing all endpoints for pickup and launching seeder [All endpoints from pickup working]

--- src/database/seeder.js
+++ src/database/seeder.js
@@ -1,10 +1,12 @@
 
 import { seederDefaultCharacters } from "./seeders/seederCharacters.js";
 import { seederDefaultItems } from "./seeders/seederItems.js";
+import { seederDefaultPickups } from "./seeders/seederPickups.js";
 
 const seeder = async () => {
     await seederDefaultCharacters();
     await seederDefaultItems();
+    await seederDefaultPickups();
 }
 
 seeder();
