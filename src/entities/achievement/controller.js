d87c325607ee0c73411e747936d4c4b08063bad6
Caribla <caribla4@gmail.com>
2024-05-08 12:50:34 +0200

    Feat: Post achievement endpoint

--- src/entities/achievement/controller.js
+++ src/entities/achievement/controller.js
@@ -15,11 +15,42 @@ export const getAllAchievements = async (req, res) => {
 }
 
 export const postAchievement = async (req, res) => {
-    return res.status(201).json({
-        success: true,
-        message: "Achievement posted succesfully",
-        // data: achievements
-    });
+    try {
+        const { name, description, how_to, image, character_id, item_id, pickup_id } = req.body;
+
+        if(!name || !description || !how_to || !image){
+            throw new Error ("No provided correct data for post a pickup");
+        }
+
+        const newAchievementItems = {
+            name,
+            description,
+            type,
+            image
+        };
+
+        if(character_id){
+            newAchievementItems.character_id = character_id
+        };
+
+        if(item_id){
+            newAchievementItems.item_id = item_id
+        };
+
+        if(character_id){
+            newAchievementItems.pickup_id = pickup_id
+        };
+
+        const newAchievement = await Achievement.create(newAchievementItems);
+        
+        return res.status(200).json({
+            success: true,
+            message: "Achievement posted succesfully",
+            data: newAchievement
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 export const updateAchievementById = async (req, res) => {
