d36df86489836fd67137223e26ad8956cd8b5db6
Caribla <caribla4@gmail.com>
2024-05-09 12:01:04 +0200

    Feat: Seeder default marks

--- src/database/seeder.js
+++ src/database/seeder.js
@@ -3,12 +3,14 @@ import { seederDefaultCharacters } from "./seeders/seederCharacters.js";
 import { seederDefaultItems } from "./seeders/seederItems.js";
 import { seederDefaultPickups } from "./seeders/seederPickups.js";
 import { seederDefaultAchievements } from "./seeders/seederAchievements.js";
+import { seederDefaultMarks } from "./seeders/seederMarks.js";
 
 const seeder = async () => {
-    // await seederDefaultCharacters();
-    // await seederDefaultItems();
-    // await seederDefaultPickups();
+    await seederDefaultCharacters();
+    await seederDefaultItems();
+    await seederDefaultPickups();
     await seederDefaultAchievements();
+    await seederDefaultMarks();
 }
 
 seeder();
